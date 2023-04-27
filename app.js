2de13a2c1e52ece10fe2a4b097501254f185ce91
runuhway <132015073+runuhway@users.noreply.github.com>
2023-04-27 13:34:35 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+// Get the HTML elements
+const scanBtn = document.querySelector("#scan-btn");
+const resultDiv = document.querySelector("#result-div");
+
+// Add a click event listener to the scan button
+scanBtn.addEventListener("click", async () => {
+  try {
+    // Get access to the device camera
+    const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+
+    // Create a new instance of the ZXing barcode reader
+    const codeReader = new ZXing.BrowserBarcodeReader();
+
+    // Decode the barcode image from the camera stream
+    const result = await codeReader.decodeFromStream(stream);
+
+    // Update the content of the result div with the barcode data
+    resultDiv.textContent = result.text;
+
+    // Stop the camera stream
+    stream.getTracks().forEach((track) => track.stop());
+  } catch (error) {
+    console.error(error);
+  }
+});

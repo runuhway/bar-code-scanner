4174b94d2508791c66d945a221bbb966e9690322
runuhway <132015073+runuhway@users.noreply.github.com>
2023-04-27 15:02:56 -0700

    Update app.js

--- app.js
+++ app.js
@@ -4,19 +4,26 @@ const resultDiv = document.querySelector("#result-div");
 
 const video = document.getElementById('video');
 
-navigator.mediaDevices.getUserMedia({ 
-  video: { 
-    facingMode: { 
-      exact: 'environment' // Use the back camera 
-      //exact: 'user' // Use the front camera 
-    } 
-  } 
-})
-.then(stream => {
-  video.srcObject = stream;
-})
-.catch(error => {
-  console.error(error);
+// Add a click event listener to the scan button
+scanBtn.addEventListener("click", async () => {
+  try {
+    // Get access to the device camera
+    const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+    video.srcObject = stream; // Add this line to set the stream as the source for the video element
+
+    // Create a new instance of the ZXing barcode reader
+    // const codeReader = new ZXing.BrowserBarcodeReader();
+
+    // Decode the barcode image from the camera stream
+    // const result = await codeReader.decodeFromStream(stream);
+
+    // Update the content of the result div with the barcode data
+    // resultDiv.textContent = result.text;
+
+    // Stop the camera stream
+    stream.getTracks().forEach((track) => track.stop());
+  } catch (error) {
+    // console.error(error);
+  }
+  
 });
-
-

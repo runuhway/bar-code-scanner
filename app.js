e38f40ee171c6ca6c7e4d11dedc499fe65a57b3a
runuhway <132015073+runuhway@users.noreply.github.com>
2023-04-28 11:39:48 -0700

    Update app.js

--- app.js
+++ app.js
@@ -13,17 +13,27 @@ scanBtn.addEventListener("click", async () => {
     .catch(log);
     // Create a new instance of the ZXing barcode reader
      const codeReader = new ZXing.BrowserBarcodeReader();
+    // get a reference to the video element
+     const videoElement = document.getElementById('video');
+    // start the barcode scanner
+    codeReader.decodeFromVideoDevice(null, videoElement, (result, err) => {
+  if (result) {
+    console.log(result.text); // print the barcode text to the console
+  }
+  if (err && !(err instanceof ZXing.NotFoundException)) {
+    console.error(err); // print the error to the console
+  }
     
     // Decode the barcode image from the camera stream
-     const result = await codeReader.decodeFromStream(stream);
+    // const result = await codeReader.decodeFromStream(stream);
 
     // Update the content of the result div with the barcode data
-     resultDiv.textContent = result.text;
+   //  resultDiv.textContent = result.text;
 
     // Stop the camera stream
-     stream.getTracks().forEach((track) => track.stop());
-  } catch (error) {
+    // stream.getTracks().forEach((track) => track.stop());
+ // } catch (error) {
     // console.error(error);
-  }
+  //}
   
 });
